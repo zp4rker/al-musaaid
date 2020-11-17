d912ae001489409cba3e16c0e9fa3d7422f2781c
zp4rker <acc@zp4rker.com>
2020-11-17 12:40:34 +1100

    bumped disbot version to 1.1.1

--- src/main/kotlin/com/zp4rker/almusaaid/AlMusaaid.kt
+++ src/main/kotlin/com/zp4rker/almusaaid/AlMusaaid.kt
@@ -1,7 +1,12 @@
 package com.zp4rker.almusaaid
 
 import com.zp4rker.disbot.Bot
+import com.zp4rker.disbot.command.Command
+import com.zp4rker.disbot.extenstions.on
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.TextChannel
+import net.dv8tion.jda.api.events.ReadyEvent
 
 /**
  * @author zp4rker
@@ -14,5 +19,16 @@ fun main(args: Array<String>) {
         prefix = "/"
 
         activity = Activity.listening("your commands...")
+
+        commands = listOf(object : Command(aliases = arrayOf("test"), ) {
+            override fun handle(args: Array<String>, message: Message, channel: TextChannel) {
+                channel.sendMessage("You're the best!").queue()
+            }
+        })
+    }
+
+    bot.on<ReadyEvent> {
+        println()
+        bot.logger.info("Ready to serve!")
     }
 }
