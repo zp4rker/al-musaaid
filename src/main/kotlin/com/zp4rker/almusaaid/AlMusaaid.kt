47fe9272cbaa15c289f3bf903c5cf5df5b56feae
zp4rker <acc@zp4rker.com>
2020-11-19 14:47:26 +1100

    updated to disbot 1.2.1-release

--- src/main/kotlin/com/zp4rker/almusaaid/AlMusaaid.kt
+++ src/main/kotlin/com/zp4rker/almusaaid/AlMusaaid.kt
@@ -1,12 +1,19 @@
 package com.zp4rker.almusaaid
 
+import com.zp4rker.disbot.API
 import com.zp4rker.disbot.Bot
 import com.zp4rker.disbot.command.Command
-import com.zp4rker.disbot.extenstions.on
+import com.zp4rker.disbot.extenstions.KEmbedBuilder
+import com.zp4rker.disbot.extenstions.embed
+import com.zp4rker.disbot.extenstions.event.expect
+import com.zp4rker.disbot.extenstions.event.on
+import com.zp4rker.disbot.extenstions.separator
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.TextChannel
 import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
+import net.dv8tion.jda.api.requests.GatewayIntent
 
 /**
  * @author zp4rker
@@ -20,6 +27,8 @@ fun main(args: Array<String>) {
 
         activity = Activity.listening("your commands...")
 
+        intents = GatewayIntent.ALL_INTENTS
+
         commands = listOf(object : Command(aliases = arrayOf("test")) {
             override fun handle(args: Array<String>, message: Message, channel: TextChannel) {
                 channel.sendMessage("You're the best!").queue()
@@ -27,8 +36,8 @@ fun main(args: Array<String>) {
         })
     }
 
-    bot.on<ReadyEvent> {
-        bot.logger.info("=".repeat(30))
+    API.on<ReadyEvent> {
+        bot.logger.separator()
         bot.logger.info("Ready to serve!")
     }
 }
