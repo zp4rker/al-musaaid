162822a8248204556985d19468e8bf59a5a3a4ab
zp4rker <acc@zp4rker.com>
2020-11-19 23:21:07 +1100

    made http request capable of headers and content/data input

--- src/main/kotlin/com/zp4rker/almusaaid/http/HttpRequests.kt
+++ src/main/kotlin/com/zp4rker/almusaaid/http/HttpRequests.kt
@@ -7,12 +7,27 @@ import java.net.URL
  * @author zp4rker
  */
 
-fun request(method: String, baseUrl: String, parameters: Map<String, Any>): String {
-    val url = URL("$baseUrl${parameters.map { "${it.key}=${it.value}" }.joinToString("&", "?")}")
+fun request(method: String, baseUrl: String, parameters: Map<String, Any> = mapOf(), headers: Map<String, String> = mapOf(), content: String? = null): String {
+    val url = URL("$baseUrl${if (parameters.isNotEmpty()) parameters.map { "${it.key}=${it.value}" }.joinToString("&", "?") else ""}")
 
     with(url.openConnection() as HttpURLConnection) {
         requestMethod = method.toUpperCase()
 
-        return inputStream.reader().readText()
+        headers.forEach { addRequestProperty(it.key, it.value) }
+
+        content?.let {
+            doOutput = true
+
+            outputStream.use { os ->
+                with(os.writer()) {
+                    write(content)
+                    close()
+                }
+            }
+        }
+
+        inputStream.use {
+            return it.reader().run { readText().also { close() } }
+        }
     }
 }
