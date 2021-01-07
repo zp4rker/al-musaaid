5bd081725ca65a4ba310986b3172676324a30380
zp4rker <acc@zp4rker.com>
2021-01-08 08:29:15 +1100

    implemented reminders

--- src/main/kotlin/com/zp4rker/almusaaid/command/TestCommand.kt
+++ src/main/kotlin/com/zp4rker/almusaaid/command/TestCommand.kt
@@ -1,11 +1,9 @@
 package com.zp4rker.almusaaid.command
 
-import com.zp4rker.almusaaid.Trello
 import com.zp4rker.discore.command.Command
 import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.TextChannel
-import org.json.JSONObject
 
 /**
  * @author zp4rker
@@ -13,22 +11,14 @@ import org.json.JSONObject
 object TestCommand : Command(aliases = arrayOf("test"), permission = Permission.ADMINISTRATOR) {
 
     override fun handle(args: Array<String>, message: Message, channel: TextChannel) {
-        val result = Trello.getBoards(Trello.getSelfMember().getString("id")).map { it as JSONObject }.filter {
-            !it.getBoolean("closed") && Trello.getMembers(it.getString("id")).length() == 1
+        val regex = Regex("(\\d+)\\w+")
+        val content = args.joinToString(" ")
+
+        channel.sendMessage("${regex.matchEntire(content) != null}").queue()
+        regex.matchEntire(content)?.let {
+            it.groupValues.forEach { g ->
+                channel.sendMessage(g).queue()
+            }
         }
-        channel.sendMessage("${result.size}").queue()
-        channel.sendMessage(result.joinToString { it.getString("name") }).queue()
-
-        val ideasBoard = result.find { it.getString("name") == "Ideas" } ?: return
-        val lists = Trello.getLists(ideasBoard.getString("id"), "none").map { it as JSONObject }
-
-        channel.sendMessage("${lists.size}").queue()
-        channel.sendMessage(lists.joinToString { it.getString("name") }).queue()
-
-        val rawIdeasList = lists.find { it.getString("name") == "Raw Ideas" } ?: return
-
-        channel.sendMessage(rawIdeasList.getString("id")).queue()
-
-        Trello.createCard(rawIdeasList.getString("id"), "Test card", "Test description")
     }
 }
---
+++ src/main/kotlin/com/zp4rker/almusaaid/listener/ReminderListener.kt
@@ -0,0 +1,60 @@
+package com.zp4rker.almusaaid.listener
+
+import com.zp4rker.discore.API
+import com.zp4rker.discore.extenstions.event.on
+import com.zp4rker.discore.util.unicodify
+import net.dv8tion.jda.api.entities.ChannelType
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.schedule
+import kotlin.concurrent.timer
+
+/**
+ * @author zp4rker
+ */
+object ReminderListener {
+
+    private val msgRegex = Regex("remind me to (.*) in (.*)")
+    private val timeRegex = Regex("(\\d+[ ]?[^\\d^\\s^,]+)")
+
+    fun register() {
+        API.on<MessageReceivedEvent> { e ->
+            if (e.author.asTag != "zp4rker#3333") return@on
+            val matches = msgRegex.matchEntire(e.message.contentRaw)?.groupValues?.filter { it != e.message.contentRaw } ?: return@on
+
+            e.message.addReaction(":thumbsup:".unicodify()).queue()
+
+            val task = matches[0]
+            val timeRaw = matches[1]
+
+            val timeComponents = timeRegex.findAll(timeRaw).toList().also { if (it.isEmpty()) return@on }.map { it.value }
+            val millis = componentsToMillis(timeComponents)
+
+            Timer().schedule(millis) {
+                e.channel.sendMessage("${if (e.channelType != ChannelType.PRIVATE) "${e.author.asMention} " else ""}$task").queue()
+            }
+        }
+    }
+
+    private fun componentsToMillis(components: List<String>): Long {
+        var millis = 0L
+        for (rawComp in components) {
+            val comp = rawComp.replace(" ", "")
+            val amount = Regex("(\\d+)\\w+").matchEntire(comp)!!.groupValues[1].toLong()
+            when {
+                comp.matches(Regex("\\d+(?:h(?:our[s]?)?)")) -> {
+                    millis += TimeUnit.HOURS.toMillis(amount)
+                }
+                comp.matches(Regex("\\d+(?:m(?:inute[s]?)?)")) -> {
+                    millis += TimeUnit.MINUTES.toMillis(amount)
+                }
+                comp.matches(Regex("\\d+(?:s(?:econd[s]?)?)")) -> {
+                    millis += TimeUnit.SECONDS.toMillis(amount)
+                }
+            }
+        }
+        return millis
+    }
+
+}
